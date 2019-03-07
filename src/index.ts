aeedf149114c2c55090689522dae5a783eb320b5
Collins <Forrest.Collins@insight.com>
2019-03-07 16:30:09 -0600

    Adds an index ts file and some type defs

---
+++ src/index.ts
@@ -0,0 +1,14 @@
+<reference path="./index.d.ts"/>
+
+const App = () => `
+	<div>
+		<p>App</p>
+	</div>
+`
+
+function renderApp() {
+	const root = document.getElementById('root') as HTMLElement
+	root.innerHTML = App()
+}
+
+renderApp()
