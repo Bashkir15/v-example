af7feb080dae600d9d3afadf1c8e5269bc3d63bd
Collins <Forrest.Collins@insight.com>
2019-03-07 17:00:51 -0600

    Makes the cli executable

--- scripts/cli.js
+++ scripts/cli.js
@@ -1,6 +1,7 @@
 const commandLineCommands = require('command-line-commands')
 const commandLineArgs = require('command-line-args')
 
+const commands = require('./commands')
 const getOptions = require('./options')
 const schema = require('./schema')
 
@@ -11,6 +12,16 @@ function execute() {
 	const args = commandLineArgs(schema[command].options, { argv })
 	const options = getOptions(args)
 
-	// This is where we need to execute the scripts
+	try {
+		commands[command](options)
+	} catch (err) {
+		console.error('Error')
+		if (err.stack) {
+			console.error(err.stack)
+		}
 
+		process.exit(1)
+	}
 }
+
+execute()
---
+++ scripts/commands.js
@@ -0,0 +1,5 @@
+const watch = require('./commands/watch')
+
+module.exports = {
+	watch
+}
